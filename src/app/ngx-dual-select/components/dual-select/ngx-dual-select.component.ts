81e84738064eb976545fb737577057f38f269379
Fillipe Araujo <fillipesouzaaraujo@gmail.com>
2019-12-30 19:46:08 +0100

    Css Formater

--- src/app/ngx-dual-select/components/dual-select/ngx-dual-select.component.ts
+++ src/app/ngx-dual-select/components/dual-select/ngx-dual-select.component.ts
@@ -2,10 +2,17 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-ngx-dual-select',
+  inputs: [
+    "leftTitle",
+    "rightTitle"
+  ],
   templateUrl: './ngx-dual-select.component.html',
   styleUrls: ['./ngx-dual-select.component.scss']
 })
 export class NgxDualSelectComponent implements OnInit {
+
+  leftTitle: string;
+  rightTitle: string;
 
   constructor() { }
 
