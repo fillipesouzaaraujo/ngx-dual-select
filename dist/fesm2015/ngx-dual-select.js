fda75e0c29c93fe4cc6545dfd031a23c03fd2dfe
Fillipe Araujo <fillipesouzaaraujo@gmail.com>
2019-12-30 19:48:33 +0100

    Compile

--- dist/fesm2015/ngx-dual-select.js
+++ dist/fesm2015/ngx-dual-select.js
@@ -10,8 +10,12 @@ let NgxDualSelectComponent = class NgxDualSelectComponent {
 NgxDualSelectComponent = __decorate([
     Component({
         selector: 'app-ngx-dual-select',
-        template: "<p>\n  ngx-dual-select works!\n</p>\n<p>\n  ngx-dual-select works!\n</p><p>\n  ngx-dual-select works!\n</p><p>\n  ngx-dual-select works!\n</p>",
-        styles: [""]
+        inputs: [
+            "leftTitle",
+            "rightTitle"
+        ],
+        template: "<div class=\"container\">\n  <div class=\"box left\">\n    <h1 class=\"title\">{{leftTitle}}</h1>\n  </div>\n  <div class=\"box center\">\n    <div class=\"actions\">\n      <button type=\"button\">&gt;&gt;</button>\n      <button type=\"button\">&gt;</button>\n      <hr />\n      <button type=\"button\">&lt;</button>\n      <button type=\"button\">&lt;&lt;</button>\n    </div>\n  </div>\n  <div class=\"box right\">\n    <h1 class=\"title\">{{rightTitle}}</h1>\n  </div>\n</div>",
+        styles: [".container{display:-webkit-box;display:flex;flex-wrap:wrap;-webkit-box-align:top;align-items:top;-webkit-box-pack:center;justify-content:center;background:#f8f8f8}.container .box{-webkit-box-flex:1;flex-grow:1;flex:1 1 30%;height:auto}.container .box .title{font-size:14px;font-weight:700;font-family:Arial,Helvetica,sans-serif}.container .box.left,.container .box.right{padding:10px}.container .box.center{-webkit-box-flex:1;flex:1 1 10%;border-right:1px solid #d4d4d4;border-left:1px solid #d4d4d4}.container .box hr{border-top:1px solid #e4e4e4;height:0;margin-top:20px;margin-bottom:20px}.container .box .actions{padding-top:20px;padding-left:20px;padding-right:20px}.container .box .actions button{display:block;width:100%;margin-bottom:10px;background:#eee;border-radius:3px;font-size:16px}"]
     })
 ], NgxDualSelectComponent);
 
--- dist/src/app/ngx-dual-select/components/dual-select/ngx-dual-select.component.d.ts
+++ dist/src/app/ngx-dual-select/components/dual-select/ngx-dual-select.component.d.ts
@@ -1,5 +1,7 @@
 import { OnInit } from '@angular/core';
 export declare class NgxDualSelectComponent implements OnInit {
+    leftTitle: string;
+    rightTitle: string;
     constructor();
     ngOnInit(): void;
 }
