14f5ed7c8a5d572bca726e583b50c5ae670f6dc9
Fillipe Araujo <fillipesouzaaraujo@gmail.com>
2019-12-31 13:07:05 +0100

    Create objects and interfaces

--- src/app/ngx-dual-select/components/dual-select/ngx-dual-select.component.ts
+++ src/app/ngx-dual-select/components/dual-select/ngx-dual-select.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+
+export interface Item {
+  title: string;
+  subtitle?: string;
+  active: boolean
+}
+
 @Component({
   selector: 'app-ngx-dual-select',
   inputs: [
@@ -13,10 +20,60 @@ export class NgxDualSelectComponent implements OnInit {
 
   leftTitle: string;
   rightTitle: string;
+  listItems: Item[] = [];
+  listItemsSelected: Item[] = [];
 
   constructor() { }
 
   ngOnInit() {
+    this.mockLoadItems();
   }
 
+  private mockLoadItems() {
+    this.listItems.push(
+      { title: 'SETOR - CONTROLADORIA GERAL', subtitle: '2893 Usuários Activos', active: false },
+      { title: 'SETOR - CONTROLADORIA DE PROCESSOS', subtitle: '2893 Usuários Activos', active: false }
+    );
+  }
+
+
+  selectItem(item: Item) {
+    item.active = !item.active;
+  }
+
+  moveSelected(direction: string) {
+    switch (direction) {
+      case 'right':
+        this.moveSelectedRight();
+        break;
+      case 'left':
+        this.moveSelectedLeft();
+        break;
+    }
+  }
+
+
+  private moveSelectedRight() {
+    this.listItems.forEach((item, key) => {
+      if (item.active) {
+        if (this.listItemsSelected.indexOf(item) <= -1) {
+          item.active = false;
+          this.listItemsSelected.push(item);
+          this.listItems.splice(key, 1);
+        }
+      }
+    });
+  }
+
+  private moveSelectedLeft() {
+    this.listItemsSelected.forEach((item, key) => {
+      if (item.active) {
+        if (this.listItems.indexOf(item) <= -1) {
+          item.active = false;
+          this.listItems.push(item);
+          this.listItemsSelected.splice(key, 1);
+        }
+      }
+    });
+  }
 }
